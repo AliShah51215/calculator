29e3b077ea07e8a63921d9dede9f053c88dfb977
Ali <alishah152001@gmail.com>
2021-08-26 19:09:19 +0200

    first attempt with decimal bugs

--- src/main/java/pl/shah/App.java
+++ src/main/java/pl/shah/App.java
@@ -1,13 +1,44 @@
 package pl.shah;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        String yn;
+
+        do
+        {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter First No.");
+            double no1 = s.nextDouble();
+            System.out.println("Enter Second No.");
+            double no2 = s.nextDouble();
+            System.out.println("Select Symbol(+,-,*,/)");
+            String sym = s.next();
+            double res;
+            switch (sym) {
+                case "+" -> {
+                    res = no1 + no2;
+                    System.out.println("Addition is : " + res);
+                } case "-" -> {
+                    res = no1 - no2;
+                    System.out.println("Subtraction is : " + res);
+                }
+                case "*" -> {
+                    res = no1 * no2;
+                    System.out.println("Multiplication is : " + res);
+                }
+                case "/" -> {
+                    res = no1 / no2;
+                    System.out.println("Division is : " + res);
+                }
+                default -> System.out.println("Invalid Symbol");
+            }
+            System.out.println("Do You Want To Continue(Press y for Yes and n for No)");
+            yn=s.next();
+
+        } while (yn.equals("y")||yn.equals("Y"));
     }
+
+
 }
