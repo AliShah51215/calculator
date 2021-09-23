afca9e40e61eeab4cf03931215dd3d41c872c1e6
Ali <alishah152001@gmail.com>
2021-09-23 20:40:16 +0200

    Lesson 4 - OOP 2 - Activity 5 - Animal class

---
+++ src/main/java/Animal/Dog.java
@@ -0,0 +1,16 @@
+package Animal;
+
+public class Dog extends Animal {
+    public Dog(String name,
+               String race,
+               String age,
+               String color) {
+        super(name,
+                race,
+                age,
+                color);
+
+    }
+
+}
+
