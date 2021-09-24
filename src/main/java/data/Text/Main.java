0b300fbe152c610b111593358ad6cbe6f13207fa
Ali <alishah152001@gmail.com>
2021-09-25 01:59:47 +0200

    Lesson 6 - Files, Exceptions - Activity 0 - data.txt

---
+++ src/main/java/data/Text/Main.java
@@ -0,0 +1,30 @@
+package data.Text;
+
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try {
+            File file = new File("data.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IIOException exception) {
+            exception.printStackTrace();
+        }
+
+        String pathToFile = "C:\\Users\\Ali\\IdeaProjects\\Github\\target\\data.txt";
+        String text = "The Course\n" +
+                "Java\n" +
+                "Lesson 6\n" +
+                "Files\n" +
+                "Exceptions\n" +
+                "Files\n" +
+                "End Of Files";
+
+        TextFileWriter textFileWriter = new TextFileWriter();
+        textFileWriter.writeToFile(pathToFile, text);
+    }
+}
