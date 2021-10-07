68ff0fa90dd3b928089cea361add9c9a5de90c3e
Ali <alishah152001@gmail.com>
2021-10-07 14:43:16 +0200

    Mockito - Activity 1 - Numbers

---
+++ src/main/java/numbers_divisible/Main.java
@@ -0,0 +1,8 @@
+package numbers_divisible;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("is Divisible By 2 :" + Numbers.isDivisibleBy2(100));
+        System.out.println("sum of the digits of a given number :" + Numbers.sumOfDigits(432));
+    }
+}
---
+++ src/main/java/numbers_divisible/Numbers.java
@@ -0,0 +1,25 @@
+package numbers_divisible;
+
+public class Numbers {
+    public static boolean isDivisibleBy2(int num1) {
+        return num1 % 2 == 0;
+
+    }
+
+    public static int sumOfDigits(int num2) {
+        int sum = 0;
+
+        if (num2 < 0) {
+            num2 = -num2;
+        }
+
+        while (num2 > 0) {
+            sum += num2 % 10;
+            num2 /= 10;
+        }
+
+        return sum;
+    }
+
+
+    }
