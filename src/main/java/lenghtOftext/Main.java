5248f798fb5b79d1009ed3d8f868230c1a8c7f6c
Ali <alishah152001@gmail.com>
2021-10-07 17:21:49 +0200

    Activity 4 - Length of the text

---
+++ src/main/java/lenghtOftext/Main.java
@@ -0,0 +1,7 @@
+package lenghtOftext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Text Length is: " + TextLength.getTextLength("Massachusetts"));
+    }
+}
