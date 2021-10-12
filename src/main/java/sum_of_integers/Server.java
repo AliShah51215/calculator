efb025a75c5ab3a67a4f3617691a2c33dd89a4b1
Ali <alishah152001@gmail.com>
2021-10-12 20:21:26 +0200

    Lesson 12 - Multithreading - Activity 3 - Sum of integers

---
+++ src/main/java/sum_of_integers/Server.java
@@ -0,0 +1,31 @@
+package sum_of_integers;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.Instant;
+import java.util.List;
+
+public class Server {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket serverSocket = new ServerSocket(1080);
+        System.out.println(Instant.now() + " Server is up");
+        System.out.println(Instant.now() + " Server is waiting for a request");
+
+        Socket accept = serverSocket.accept();
+        System.out.println(Instant.now() + " Server is processing a request...");
+
+        ObjectInputStream stream = new ObjectInputStream(accept.getInputStream());
+        @SuppressWarnings("unchecked")
+        List<Integer> list = (List<Integer>) stream.readObject();
+        Integer sum = list.stream().reduce(0, Integer::sum);
+        System.out.println(Instant.now() + " Server got list: " + list);
+
+        ObjectOutputStream outputStream = new ObjectOutputStream(accept.getOutputStream());
+        String messageToClient = "Sum: " + sum;
+        outputStream.writeObject(messageToClient);
+    }
+}
+
