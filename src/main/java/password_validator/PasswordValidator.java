4ad915ef77e94d30d7d9c56cd711fa0ccb4c3209
Ali <alishah152001@gmail.com>
2021-10-14 19:04:06 +0200

    Multithreading - Activity 1 - Writing a website

--- src/main/java/password_validator/PasswordValidator.java
+++ src/main/java/password_validator/PasswordValidator.java
@@ -5,14 +5,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
-    private static final String year = String.valueOf(LocalDateTime.now().getYear());
-    private static final String year2 = year.substring(2,4);
-    private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;',?/*~$^+=<>]).{7,20}$";
+    private static String year = String.valueOf(LocalDateTime.now().getYear());
+    private static String year2 = year.substring(2,4);
+    private static final Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=\\S+$).{7,}$");
 
-    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
-    public static boolean isValid(final String password) {
+    public static boolean isValid(String password) {
         if (password.contains(year) || password.contains(year2)) {
             return false;
         } else {
--- src/test/java/password_validator/PasswordValidatorTest.java
+++ src/test/java/password_validator/PasswordValidatorTest.java
@@ -1,51 +1,45 @@
 package password_validator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+class PasswordValidatorTest {
 
-public class PasswordValidatorTest {
-
-    @ParameterizedTest(name = "#{index} - Run test with password = {0}")
+    @ParameterizedTest
     @MethodSource("validPasswordProvider")
-    void test_password_regex_valid(String password) {
-        assertTrue(PasswordValidator.isValid(password));
+    void shouldVerifyPasswordIsValid(String password) {
+        boolean isValid = PasswordValidator.isValid(password);
+        Assertions.assertTrue(isValid);
     }
 
-    @ParameterizedTest(name = "#{index} - Run test with password = {0}")
+    @ParameterizedTest
     @MethodSource("invalidPasswordProvider")
-    void test_password_regex_invalid(String password) {
-        assertFalse(PasswordValidator.isValid(password));
+    void shouldVerifyPasswordIsNOTValid(String password) {
+        boolean isValid = PasswordValidator.isValid(password);
+        Assertions.assertFalse(isValid);
     }
 
     static Stream<String> validPasswordProvider() {
         return Stream.of(
-                "AAAbbbccc@123",
-                "Hello world$123",
-                "A!@#&()–a1",
-                "A[{}]:;',?/*a1",
-                "A~$^+=<>a1",
-                "0123456789$abcdefgAB",
-                "123Aa$A"
+                "gyyufuyF666TDRDTDju",
+                "123AaAa",
+                "000000M",
+                "1abcDef",
+                "675765R674"
         );
     }
+
     static Stream<String> invalidPasswordProvider() {
         return Stream.of(
-                "2021@Test",                 //test year
-                "12345678",                 // invalid, only digit
-                "abcdefgh",                 // invalid, only lowercase
-                "ABCDEFGH",                 // invalid, only uppercase
-                "abc123$$$",                // invalid, at least one uppercase
-                "ABC123$$$",                // invalid, at least one lowercase
-                "ABC$$$$$$",                // invalid, at least one digit
-                "java REGEX 123",           // invalid, at least one special character
-                "java REGEX 123 %",         // invalid, % is not in the special character group []
-                "________",
-                "--------",
-                " ");
+                "gyyufuy666ju",
+                "123AAA",
+                "te  st",
+                "Qwerty21",
+                "2021test"
+
+        );
     }
 }
