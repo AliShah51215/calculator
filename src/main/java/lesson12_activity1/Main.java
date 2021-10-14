16570b447dfed24091cf083191219709b347313c
Ali <alishah152001@gmail.com>
2021-10-14 15:45:30 +0200

    Multithreading - Activity 1 - Writing a website

---
+++ src/main/java/lesson12_activity1/Main.java
@@ -0,0 +1,21 @@
+package lesson12_activity1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("https://www.oracle.com/index.html");
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()))) {
+            try (PrintWriter writer = new PrintWriter("oracle.html")) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    writer.write(inputLine);
+                }
+            }
+        }
+    }
+}
